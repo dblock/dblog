bf534d68ca1544e433c50f9c26d34c9a974f1d67
dB <dblock@dblock.org>
2008-10-20 12:58:54 +0000

    Added Data.Mapping tool and processed output.

---
+++ Data.Mapping/AdditionalProjectFiles.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace DBlog.Data.Mapping
+{
+    public enum AdditionalProjectFileConfigurationSubType
+    {
+        File,
+        Link
+    }
+
+    public class AdditionalProjectFileConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("filename", IsRequired = true)]
+        public String Filename
+        {
+            get
+            {
+                return (String)this["filename"];
+            }
+            set
+            {
+                this["filename"] = value;
+            }
+        }
+
+        [ConfigurationProperty("subtype", IsRequired = false)]
+        public AdditionalProjectFileConfigurationSubType SubType
+        {
+            get
+            {
+                return (AdditionalProjectFileConfigurationSubType)this["subtype"];
+            }
+            set
+            {
+                this["subtype"] = value;
+            }
+        }
+    }
+
+    public class AdditionalProjectFileCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AdditionalProjectFileConfigurationElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AdditionalProjectFileConfigurationElement) element).Filename;
+        }
+    }
+
+    public class AdditionalProjectFilesConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true, IsRequired = true)]
+        public AdditionalProjectFileCollection AdditionalProjectFiles
+        {
+            get
+            {
+                return (AdditionalProjectFileCollection) this[""];
+            }
+        }
+    }
+}
---
+++ Data.Mapping/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DBlog.Data.Mapping")]
+[assembly: Guid("932f5bba-21aa-4d8e-850a-902630f6285e")]
